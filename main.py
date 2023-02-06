3b731575a70723dcb30520540c4598c01a586161
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2023-02-06 11:55:30 +0900

    [PJWP] main file
    It is a file with functions that implement multiple functions.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
+from pyqtgraph import PlotWidget, plot
 from PyQt5 import uic
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -17,7 +17,7 @@ def resource_path(relative_path): # 파일과 리소스의 경로 찾기
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path) # 파일의 총 경로를 반환
 
-UI_form = resource_path('/Users/bdh/Desktop/PJWP/Again/re_MainWindow.ui') # ui 파일의 총 경로를 form에 할당
+UI_form = resource_path('./MainWindow.ui') # ui 파일의 총 경로를 form에 할당
 UI_form_class = uic.loadUiType(UI_form)[0] # 해당 파일을 uic class형태로 변환
 
 
@@ -29,12 +29,12 @@ class MainWindow(QMainWindow, UI_form_class): #QMainWindow와 ui를 변환한 cl
         self.setAcceptDrops(True)
         
 
-        self.tree_fields = []           # 트리에있는 전체 데이터가 들어있는 영역
-        self.time = 0                       # 시간 초기환
-        self.time_started = False           # Start, Stop Button
-        self.wnum = 0                       # create window 클릭 횟수 저장소
-        self.graph_window = []              # graph window 몇번 째인지
-        self.recent_data = []               # 처음 불러들인 데이터의 columns이름 저장소
+        self.tree_fields = []                               # 트리에있는 전체 데이터가 들어있는 영역
+        self.time = 0                                       # 시간 초기환
+        self.time_started = False                           # Start, Stop Button
+        self.wnum = 0                                       # create window 클릭 횟수 저장소
+        self.graph_window = []                              # graph window 몇번 째인지
+        self.recent_data = []                               # 처음 불러들인 데이터의 columns이름 저장소
         
         
         # Load File 버튼이나 메뉴바 action
@@ -44,13 +44,16 @@ class MainWindow(QMainWindow, UI_form_class): #QMainWindow와 ui를 변환한 cl
         # 새로운 윈도우 창 버튼 클릭          
         self.btn_Create.clicked.connect(self.create_graph_window)
         self.actionCreate_Graph_Window.triggered.connect(self.create_graph_window)
-    
+
+        # 그래프들 동기화 버튼
         self.btn_Syn.clicked.connect(self.InfiniteLine)
         self.actionSyn.triggered.connect(self.InfiniteLine)
         
+        # 동기화된 그래프들 분석 시작~
         self.btn_Start.clicked.connect(self.start_movement)
         self.actionStart_Time.triggered.connect(self.start_movement)
         
+        # 동기화된 그래프들 잠깐 멈춰!!
         self.btn_Stop.clicked.connect(self.stop_movement)
         self.actionStop_Time.triggered.connect(self.stop_movement)
 
@@ -75,7 +78,7 @@ class MainWindow(QMainWindow, UI_form_class): #QMainWindow와 ui를 변환한 cl
     def setup_loaded_data(self):
         for name in self.data.columns:
             self.recent_data.append(name)
-            self.tree_fields.append(['{}'.format(name)])         # self.tree_fields안에 트리에 있는 데이터 columns이름들이 들어있다
+            self.tree_fields.append(name)                   # self.tree_fields안에 트리에 있는 데이터 columns이름들이 들어있다
         self.update_selection_list()    
 
     # load한 csv의 columns의 목록들을 tree에 표시하는 함수
@@ -97,47 +100,65 @@ class MainWindow(QMainWindow, UI_form_class): #QMainWindow와 ui를 변환한 cl
         self.graph_window.append(graph)
         self.graph_window[self.wnum-1].show()
     
-    
+    # 그래프 그리기
     def Plot(self):
         self.plot_widget = pg.PlotWidget()
+        self.plot_widget.setBackground('w')
         self.plot_widget.setTitle("{} Graph".format(self.selected))
         self.plot_widget.showGrid(x=True, y=True)
         data = self.data[self.selected]
         self.x = []
+        self.curve = []
         for i in range(len(data)):
             self.x.append(i)
-            
-        self.y = data
-        self.plot_widget.plot(self.x, self.y)
         
+        self.y = data
+        
+        self.plot_widget.plot(self.x, self.y)
+        self.curve.append(self.plot_widget.plotItem.curves[0])
         return self.plot_widget        
     
+    # 동기화 선 그리기
     def InfiniteLine(self):
         self.lines = []
+        self.labels = []
         for i in range(self.wnum):
             self.line = pg.InfiniteLine(angle=90, movable=False)
             self.graph_window[i].addItem(self.line)
             self.lines.append(self.line)
-
+            
+            self.line_value_label = QLabel(self)
+            self.line_value_label.move(100, 100)
+            self.labels.append(self.line_value_label)
+            
         self.x_val = 0
         self.timer = pg.QtCore.QTimer()
         self.timer.timeout.connect(self.move_line)
         
+    # Start to move InfiniteLine
     def start_movement(self):
         self.timer.start(100)
-        
+    
+    # Stop moving InfiniteLine
     def stop_movement(self):
         if self.timer.isActive():
             self.timer.stop()
         else:
             self.timers.start()
-        
+    
+    # InfiniteLine Movement Setting
     def move_line(self):
         self.x_val += 0.1
         for i in range(self.wnum):
             self.lines[i].setValue(self.x_val)
-        
-    
+            # Slider setting
+            self.Slider.setRange(0, len(self.data)*10)
+            self.Slider.setValue(self.Slider.value()+1)
+            # y좌표 설정
+            self.y_val = self.curve[i-1].getData()[1][np.argmin(np.abs(self.curve[i-1].getData()[0] - self.lines[i].value()))]
+            self.labels[i].setText("Line value: {}".format(self.y_val))
+            self.labels[i].show()
+            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # app 생성
